40302c80b84569be9177347c406be16261a0539c
jeffcail <cljcailianjie@163.com>
2023-05-20 00:46:40 +0800

    引入asios实现增删改查

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div height="100%">
     <el-container style="line-height: 100%;">
-      <el-aside :width="asideWidth+'px'" style="background-color: rgb(238, 241, 246); line-height: 100%;">
+      <el-aside :width="asideWidth+'px'" style="background-color: rgb(238, 241, 246); line-height: 100%; height: 100vh; overflow-x: hidden;">
         <el-menu :default-openeds="['1', '3']" style="height: 100%; overflow-x: hidden;" 
                   background-color="rgb(48, 65, 86)"
                   text-color="#fff"
@@ -94,20 +94,25 @@
         <el-main>
 
           <div style="padding: 10px 0">
-            <el-input style="width: 200px" placeholder="请输入搜索的内容" v-model="username"></el-input>
-            <!-- <el-input style="width: 200px" placeholder="请输入搜索的内容" class="ml-5"></el-input>
-            <el-input style="width: 200px" placeholder="请输入搜索的内容" class="ml-5"></el-input> -->
+            <el-input style="width: 200px" placeholder="请输入搜索的用户名" v-model="username"></el-input>
+            <el-input style="width: 200px; margin-left: 5px;" placeholder="请输入搜索的昵称" v-model="nickname"></el-input>
+            <el-input style="width: 200px; margin-left: 5px;" placeholder="请输入搜索的邮箱" v-model="email"></el-input>
+            <el-input style="width: 200px; margin-left: 5px;" placeholder="请输入搜索的地址" v-model="address"></el-input>
+            <el-input style="width: 200px; margin-left: 5px;" placeholder="请输入搜索的手机号" v-model="phone"></el-input>
             <el-button class="ml-5" type="primary" @click="searchUser">搜索</el-button>
+            <el-button class="ml-5" type="warning" @click="reset">重置</el-button>
           </div>
 
           <div style="margin: 10px;">
-              <el-button type="primary">添加<i class="el-icon-circle-plus-outline"></i></el-button>
-              <el-button type="danger">批量删除<i class="el-icon-remove-outline"></i></el-button>
+              <el-button type="primary" @click="handleAdd">添加<i class="el-icon-circle-plus-outline"></i></el-button>
+              <el-button type="danger" @click="handleDatchDel">批量删除<i class="el-icon-remove-outline"></i></el-button>
               <el-button type="success">导入<i class="el-icon-top"></i></el-button>
               <el-button type="success">导出<i class="el-icon-bottom"></i></el-button>
           </div>
 
-          <el-table :data="tableData">
+          <el-table :data="tableData"  @selection-change="handleSelectionChange">
+            <el-table-column type="selection" width="55">
+            </el-table-column>
             <el-table-column prop="id" label="编号" width="140">
             </el-table-column>
             <el-table-column prop="username" label="用户名" width="140">
@@ -121,8 +126,11 @@
             <el-table-column prop="address" label="地址">
             </el-table-column>
             <el-table-column label="操作">
-              <el-button type="success">编辑<i class="el-icon-edit"></i></el-button>
-              <el-button type="danger">删除<i class="el-icon-remove-outline"></i></el-button>
+              <template slot-scope="scope">
+                <el-button type="success" @click="handleEdit(scope.row)">编辑<i class="el-icon-edit"></i></el-button>
+
+                <el-button type="danger" @click="handleDel(scope.row.id)">删除<i class="el-icon-remove-outline"></i></el-button>
+              </template>
             </el-table-column>
           </el-table>
 
@@ -137,6 +145,40 @@
               :total="total">
             </el-pagination>
           </div>
+
+          <!-- add -->
+          <el-dialog title="用户信息" :visible.sync="dialogFormVisible">
+            <el-form :model="form" label-width="120px">
+              <el-form-item label="用户名">
+                <el-input v-model="form.username" autocomplete="off"></el-input>
+              </el-form-item>
+
+              <el-form-item label="密码">
+                <el-input v-model="form.password" autocomplete="off" type="password"></el-input>
+              </el-form-item>
+
+              <el-form-item label="昵称">
+                <el-input v-model="form.nickname" autocomplete="off"></el-input>
+              </el-form-item>
+
+              <el-form-item label="邮箱">
+                <el-input v-model="form.email" autocomplete="off"></el-input>
+              </el-form-item>
+
+              <el-form-item label="手机号">
+                <el-input v-model="form.phone" autocomplete="off"></el-input>
+              </el-form-item>
+
+              <el-form-item label="地址">
+                <el-input v-model="form.address" autocomplete="off"></el-input>
+              </el-form-item>
+              
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+              <el-button @click="dialogFormVisible = false">取 消</el-button>
+              <el-button type="primary" @click="save">确 定</el-button>
+            </div>
+        </el-dialog>
 
         </el-main>
       </el-container>
@@ -161,6 +203,13 @@ export default {
         asideWidth: 200,
         logoTextShow: true,
         username: '',
+        email: '',
+        nickname: '',
+        address: '',
+        phone: '',
+        dialogFormVisible: false,
+        form: {},
+        visible: false,
       }
     },
 
@@ -188,10 +237,21 @@ export default {
     },
 
     load() {
-      fetch("http://localhost:9000/user/list/page?pageNum="+this.pageNum+"&pageSize="+this.pageSize+"&username="+this.username).then(res => res.json()).then(res => {
-        this.tableData = res.data;
-        this.total = res.total;
-      });
+      this.request.get("/user/list/page", {
+        params: {
+          pageNum: this.pageNum,
+          pageSize: this.pageSize,
+          username: this.username,
+          email: this.email,
+          nickname: this.nickname,
+          address: this.address,
+          phone: this.phone,
+        }
+      }).then(res => {
+        this.tableData = res.records
+        this.total = res.total
+        // console.log(res);
+      })
     },
 
     handleCurrentChange(pageNum) {
@@ -207,6 +267,62 @@ export default {
     searchUser() {
       this.load();
     },
+    reset() {
+      this.username = "";
+      this.nickname = "";
+      this.email = "";
+      this.address = "";
+      this.phone = "";
+      this.load();
+    },
+    handleAdd() {
+      this.dialogFormVisible = true
+      this.form = {}
+    },
+    save() {
+      this.request.post("/user/save", this.form).then(res => {
+        if (res) {
+          this.$message.success("添加成功");
+          this.dialogFormVisible = false;
+          this.load();
+        } else {
+          this.$message.error("添加失败");
+          
+        }
+      })
+      
+    },
+    handleEdit(row) {
+      this.form = row
+      this.dialogFormVisible = true
+    },
+    
+    handleDel(id) {
+      this.request.delete("/user/" + id).then(res => {
+        if (res) {
+          this.$message.success("删除成功");
+          this.load();
+        } else {
+          this.$message.error("删除失败");
+        }
+      })
+    },
+    handleSelectionChange(val) {
+      this.multipleSelection = val;
+    },
+
+    handleDatchDel() {
+      let ids = this.multipleSelection.map(v => v.id);
+      this.request.post("/user/del/batch", ids).then(res => {
+        if (res) {
+          this.$message.success("删除成功");
+          this.load();
+        } else {
+          this.$message.error("删除失败");
+        }
+      })
+    },
+
   },
 }
 </script>
